2c1ba01218a2c713bc2d073079ee6b7e87cf8dc1
dont <1046866318@qq.com>
2017-04-13 18:03:38 +0800

    list组件

---
+++ src/components/list.vue
@@ -0,0 +1,160 @@
+<template>
+  <div>
+    <div class="list-wrap">
+      <div class="read-first">
+        <div class="read-first-title"><i class="el-icon-information"></i>推荐阅读</div>
+        <div class="read-first-content">
+          <a>商品发布规则</a>
+          <a>购买二手iphone等数码产品注意事项</a>
+        </div>
+      </div>
+      <ul class="list-content">
+        <li v-for="item in commodityItems">
+          <img src="static/images/list/phone.jpg" alt="">
+          <div>{{ item.name }}</div>
+          <div><span>{{ item.owner.info }}</span><span>{{ item.owner.name }}</span></div>
+          <div>¥ <span>{{ item.price }}</span></div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    data () {
+      return {
+        commodityItems: [
+          {
+            img: 'static/images/list/phone.jpg',
+            name: '欧派电动车',
+            owner: {
+              info: '通信13级',
+              name: '张三'
+            },
+            price: 2500
+          },
+          {
+            img: 'static/images/list/phone.jpg',
+            name: '欧派电动车',
+            owner: {
+              info: '通信13级',
+              name: '张三'
+            },
+            price: 2500
+          },
+          {
+            img: 'static/images/list/phone.jpg',
+            name: '欧派电动车',
+            owner: {
+              info: '通信13级',
+              name: '张三'
+            },
+            price: 2500
+          },
+          {
+            img: 'static/images/list/phone.jpg',
+            name: '欧派电动车',
+            owner: {
+              info: '通信13级',
+              name: '张三'
+            },
+            price: 2500
+          },
+          {
+            img: 'static/images/list/phone.jpg',
+            name: '欧派电动车',
+            owner: {
+              info: '通信13级',
+              name: '张三'
+            },
+            price: 2500
+          },
+          {
+            img: 'static/images/list/phone.jpg',
+            name: '欧派电动车',
+            owner: {
+              info: '通信13级',
+              name: '张三'
+            },
+            price: 2500
+          },
+          {
+            img: 'static/images/list/phone.jpg',
+            name: '欧派电动车',
+            owner: {
+              info: '通信13级',
+              name: '张三'
+            },
+            price: 2500
+          },
+          {
+            img: 'static/images/list/phone.jpg',
+            name: '欧派电动车',
+            owner: {
+              info: '通信13级',
+              name: '张三'
+            },
+            price: 2500
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .list-wrap {
+    width: 850px;
+    margin: 0 auto;
+    .read-first {
+      background-color: white;
+      margin-top: 40px;
+      padding: 20px;
+
+      .read-first-title {
+        color: rgb(100, 203, 177);
+        font-size: 20px;
+        margin-bottom: 20px;
+
+        i {
+          display: inline-block;
+          margin-right: 10px;
+        }
+
+      }
+      .read-first-content {
+        color: gray;
+        font-size: 14px;
+
+        a {
+          margin-right: 60px;
+          cursor: pointer;
+          display: inline-block;
+
+          &:hover {
+            color: rgb(100, 203, 177);
+          }
+        }
+      }
+    }
+    .list-content {
+      margin-top: 40px;
+      border-top: 1px dashed rgb(100, 203, 177);
+      padding: 40px 0;
+      width: 100%;
+      li {
+        display: inline-block;
+        width: 200px;
+        height: 270px;
+        margin-left: 10px;
+        margin-bottom: 20px;
+        img {
+          width: 200px;
+          height: 200px;
+        }
+      }
+    }
+  }
+
+</style>
---
+++ src/components/nav.vue
@@ -0,0 +1,141 @@
+<template>
+  <div>
+    <ul class="nav">
+      <li class="home-icon"><img src="static/images/nav/home.jpg" alt="首页"></li>
+      <li class="home-title">
+        <div>
+          <p>校园共享</p>
+          <p>享你所享，淘你钟意</p>
+        </div>
+      </li>
+      <li class="hone-search">
+        <div class="search-wrap">
+          <button class="search-right">搜索</button>
+          <div class="search-left">
+            <img src="static/images/nav/search-icon.png" alt="">
+            <div class="search-input"><input type="text" placeholder="搜索"></div>
+          </div>
+        </div>
+      </li>
+      <li class="home-person">
+        <el-dropdown class="dropdown">
+          <div class="el-dropdown-link">
+            <img src="static/images/nav/100.png" alt="">
+            <span>访客</span>
+          </div>
+          <el-dropdown-menu class="dropdown-menu">
+            <el-dropdown-item class="dropdown-item">嗨，访客</el-dropdown-item>
+            <el-dropdown-item class="dropdown-item">个人中心</el-dropdown-item>
+            <el-dropdown-item class="dropdown-item">我的收藏</el-dropdown-item>
+            <el-dropdown-item class="dropdown-item">退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+  .nav {
+    width: 100%;
+    height: 120px;
+    display: flex;
+    flex-flow: row nowrap;
+    background-color: white;
+    li {
+      float: left;
+      display: inline-block;
+    }
+    .home-icon {
+      height: 120px;
+      img {
+        height: 100%;
+      }
+    }
+    .home-title {
+      flex: 1;
+      text-align: center;
+      margin-top: 30px;
+      & :first-child {
+        color: rgb(100, 203, 279);
+        font-size: 26px;
+        margin-bottom: 5px;
+       }
+      & :last-child {
+          color: rgb(255, 178, 0);
+          font-size: 16px;
+       }
+    }
+    .hone-search {
+      width: 640px;
+      padding-top: 35px;
+      .search-wrap {
+        height: 50px;
+        border: 1px solid #44C1A5;
+        border-radius: 25px;
+        color: #4BC0A5;
+        overflow: hidden;
+        .search-left {
+          margin-right: 24%;
+          height: 50px;
+          img {
+            margin: 12px 0 0 20px;
+            float: left;
+          }
+          .search-input {
+            padding: 10px;
+            height: 100%;
+            margin-left: 50px;
+            input {
+              width: 100%;
+              height: 100%;
+              border: 0;
+              outline: none;
+              font-size: 16px;
+            }
+          }
+        }
+        .search-right {
+          float: right;
+          width: 24%;
+          height: 50px;
+          background-color: rgb(100, 203, 177);
+          border: 0;
+          color: white;
+          font-size: 16px;
+          cursor: pointer;
+        }
+
+      }
+    }
+    .home-person {
+      flex: 1.5;
+      text-indent: 10%;
+      margin-top: 35px;
+      .dropdown {
+        height: 50px;
+        cursor: pointer;
+        .el-dropdown-link {
+          overflow: hidden;
+          height: 50px;
+          min-width: 170px;
+        }
+        img {
+          width: 50px;
+          height: 50px;
+          border-radius: 25px;
+        }
+        span {
+          height: 50px;
+          display: inline-block;
+          line-height: 50px;
+          vertical-align: top;
+          color: rgb(100, 203, 177);
+          text-indent: 10px;
+        }
+      }
+    }
+  }
+</style>
+
+<script></script>
